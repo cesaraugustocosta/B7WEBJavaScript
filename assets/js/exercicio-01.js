edf471b8b2a32d98c439bd49ec845105609ae99c
Cesar <cesaraug76@hotmail.com>
2022-10-04 12:14:23 -0300

    Exercicio 01

---
+++ assets/js/exercicio-01.js
@@ -0,0 +1,14 @@
+// 1 - Crie uma vaariável para armazenar o nome
+// do carro e coloque o valor Ferrari
+let nomedoCarro = "Ferrari";
+console.log(nomedoCarro);
+
+// 2 - Crie uma vaariável para armazenar o preço
+// do bolo, que custa R$ 19,99. e exiba-a
+let precoBolo = 19.99;
+console.log("O preço do Bolo custa R$ " + precoBolo);
+
+// 3 - Corrija o códgio a seguir (e exiba-o).
+// let "cidade" = São Paulo
+let cidade = "São Paulo";
+console.log(cidade);
